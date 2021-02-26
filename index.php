ac5bc4b16fa757fb520f9d3031d8d07e8b4998ff
fedeodorcich <32882548+fedeodorcich@users.noreply.github.com>
2021-02-26 12:56:02 -0300

    inicio de proyecto

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+
+<!--section>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/image (1).jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/image (4).jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/image (5).jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</section-->
+
+<div class="container">
+
+
+<section class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+	<h1>Acerca de la Jineteada</h1>
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Quia dolor exercitationem fugiat cumque, sint totam fugit saepe porro sit pariatur minima enim delectus deleniti magni alias quam dolore dolores adipisci doloremque! Consequuntur totam, et nostrum culpa voluptates aspernatur maiores, deleniti facilis sequi, itaque accusamus error accusantium! Natus nobis odio, odit.</p>
+</section>
+
+
+<section class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+	<div class="col">
+		<div class="card" style="width: 18rem;">
+  			<img src="..." class="card-img-top" alt="...">
+  			<div class="card-body shadow-sm">
+    			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    			<a href="#" class="btn btn-primary">Go somewhere</a>
+  			</div>
+		</div>
+	</div>
+</section>
+</div>
+<footer></footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
