31ff73e3d5cf25825a2dc9bd44079c74db35a1c8
fedeodorcich <32882548+fedeodorcich@users.noreply.github.com>
2021-03-02 13:38:38 -0300

    news

---
+++ sucess.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body class="bg-dark">
+
+
+<form action="" class="col-md-3 p-3 container mt-4">
+   <h1>Validación</h1>
+  <div class="mb-3">
+    <label for="nombre" class="form-label">Nombre</label>
+    <input type="password" class="form-control" id="nombre" required>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">E-Mail</label>
+    <input type="password" class="form-control" id="email" required>
+  </div>
+  <div class="mb-3">
+    <label for="telefono" class="form-label">Teléfono</label>
+    <input type="password" class="form-control" id="telefono" required>
+  </div>
+  <div class="mb-3">
+    <label for="localidad" class="form-label">Localidad</label>
+    <input type="password" class="form-control" id="localidad" required>
+  </div>
+  <div class="mb-3">
+    <label for="operacion" class="form-label">Número de operación</label>
+    <input type="password" class="form-control" id="operacion" required>
+  </div>
+ 	
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="slick/slick.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
